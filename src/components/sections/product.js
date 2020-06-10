0d5e81b7cd5411229e096559aaca30aa6a7891f8
Jonathan Sibley <jonsibley@gmail.com>
2020-06-10 20:10:52 +0200

    move static files into static folder, insert product section images

--- src/components/sections/product.js
+++ src/components/sections/product.js
@@ -6,45 +6,80 @@ import Img from "gatsby-image"
 import styled from "styled-components"
 
 const Product = () => {
-  const [email, setEmail] = useState("")
-
-  const data = useStaticQuery(graphql`
-    query {
-      file(sourceInstanceName: { eq: "images" }, name: { eq: "green-skew" }) {
-        childImageSharp {
-          fluid(maxWidth: 1000) {
-            ...GatsbyImageSharpFluid_tracedSVG
-          }
-        }
-      }
-    }
-  `)
-
-  const handleSubmit = event => {
-    event.preventDefault()
-    window.location.href = `https://app.mailmasker.com/sign-up?alias=${email}`
-  }
-
   return (
-    <HeaderWrapper id="product">
+    <SectionWrapper id="product">
       <Container>
-          <HeaderTextGroup>
-            <Subtitle>Product</Subtitle>
-            <h1>
-              Your Mail Mask forwards<br />to your real email
-            </h1>
-            <h2>
-              Let's look at some examples:
-            </h2>
-          </HeaderTextGroup>
+        <HeaderTextGroup>
+          <Subtitle>Product</Subtitle>
+          <h1>
+            Your Mail Mask forwards
+            <br />
+            to your real email
+          </h1>
+          <h2>Let's look at some examples:</h2>
+        </HeaderTextGroup>
+        <ImagesContainer>
+          <FirstProductImage />
+          <SecondProductImage />
+          <ThirdProductImage />
+        </ImagesContainer>
       </Container>
-    </HeaderWrapper>
+    </SectionWrapper>
   )
 }
 
 export default Product
 
-const HeaderWrapper = styled.header`
+const ImagesContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+const FirstProductImage = styled.div`
+  width: 885px;
+  height: 100px;
+  margin: 0 0 12px 0;
+  background: url(/website/the-basics.svg) no-repeat top left;
+
+  @media (max-width: ${props => props.theme.screen.md}) {
+    width: 355px;
+    height: 210px;
+    margin: 0 0 10px 0;
+    background: url(/website/the-basics-mobile.svg) no-repeat top left;
+  }
+`
+
+const SecondProductImage = styled.div`
+  width: 885px;
+  height: 100px;
+  margin: 0 0 12px 0;
+  background: url(/website/create-masks-on-the-fly.svg) no-repeat top left;
+
+  @media (max-width: ${props => props.theme.screen.md}) {
+    width: 355px;
+    height: 211px;
+    margin: 0 0 10px 0;
+    background: url(/website/create-masks-on-the-fly-mobile.svg) no-repeat top
+      left;
+  }
+`
+
+const ThirdProductImage = styled.div`
+  width: 885px;
+  height: 100px;
+  margin: 0 0 12px 0;
+  background: url(/website/auto-stop-after-time.svg) no-repeat top left;
+
+  @media (max-width: ${props => props.theme.screen.md}) {
+    width: 355px;
+    height: 204px;
+    margin: 0 0 10px 0;
+    background: url(/website/auto-stop-after-time-mobile.svg) no-repeat top left;
+  }
+`
+
+const SectionWrapper = styled.section`
   background-color: #f8f8f8;
   padding: 160px 0 150px 0;
   position: relative;
@@ -84,112 +119,5 @@ const HeaderTextGroup = styled.div`
 
   p {
     margin-bottom: 48px;
-  }
-`
-
-const Flex = styled.div`
-  display: grid;
-  justify-content: space-between;
-  align-content: center;
-  grid-template-columns: 1fr 1fr;
-  @media (max-width: ${props => props.theme.screen.md}) {
-    grid-template-columns: 1fr;
-    grid-gap: 64px;
-  }
-`
-
-const HeaderForm = styled.form`
-  display: flex;
-  flex-direction: row;
-  padding-bottom: 16px;
-
-  @media (max-width: ${props => props.theme.screen.sm}) {
-    flex-direction: column;
-  }
-`
-
-const FormSubtitle = styled.span`
-  ${props => props.theme.font_size.xxsmall}
-`
-
-const FormSubtitleLink = styled.a`
-  color: ${props => props.theme.color.secondary};
-  padding-bottom: 1px;
-  margin-left: 8px;
-  text-decoration: none;
-  border-bottom: 1px solid ${props => props.theme.color.secondary};
-`
-
-const HeaderInput = styled.input`
-  font-weight: 500;
-  font-size: 16px;
-  color: ${props => props.theme.color.primary};
-  line-height: 42px;
-  width: 100%;
-  text-align: left;
-  height: 60px;
-  border-width: 1px;
-  border-style: solid;
-  border-color: ${props => props.theme.color.secondary};
-  border-image: initial;
-  border-radius: 4px;
-  padding: 8px 16px;
-  outline: 0px;
-  &:focus {
-    box-shadow: inset ${props => props.theme.color.secondary} 0px 0px 0px 2px;
-  }
-  @media (max-width: ${props => props.theme.screen.md}) {
-    margin-bottom: 8px;
-  }
-  @media (max-width: ${props => props.theme.screen.sm}) {
-    display: block;
-    width: 100%;
-  }
-`
-
-const HeaderButton = styled.button`
-  font-weight: 500;
-  font-size: 14px;
-  color: white;
-  letter-spacing: 1px;
-  height: 60px;
-  display: block;
-  margin-left: 8px;
-  text-transform: uppercase;
-  cursor: pointer;
-  white-space: nowrap;
-  background: ${props => props.theme.color.secondary};
-  border-radius: 4px;
-  padding: 0px 40px;
-  border-width: 0px;
-  border-style: initial;
-  border-color: initial;
-  border-image: initial;
-  outline: 0px;
-  &:hover {
-    box-shadow: rgba(110, 120, 152, 0.22) 0px 2px 10px 0px;
-  }
-  @media (max-width: ${props => props.theme.screen.md}) {
-  }
-  @media (max-width: ${props => props.theme.screen.sm}) {
-    margin-left: 0;
-  }
-`
-const ImageWrapper = styled.div`
-  justify-self: end;
-  align-self: center;
-  @media (max-width: ${props => props.theme.screen.md}) {
-    justify-self: center;
-  }
-`
-
-const StyledImage = styled(Img)`
-  width: 500px;
-  @media (max-width: ${props => props.theme.screen.md}) {
-    width: 400px;
-  }
-  @media (max-width: ${props => props.theme.screen.sm}) {
-    width: 300px;
-    display: none;
   }
 `
