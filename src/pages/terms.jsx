8773b1afa4c51cdc1e6d2b45e796311e792508b5
Jonathan Sibley <jonsibley@gmail.com>
2020-06-12 14:24:58 +0200

    updates to the terms

---
+++ src/pages/terms.jsx
@@ -0,0 +1,222 @@
+import { Container } from "../components/global"
+import Footer from "../components/sections/footer"
+import Layout from "../components/common/layout/layout"
+import Navigation from "../components/common/navigation/navigation"
+import React from "react"
+import SEO from "../components/common/layout/seo"
+import styled from "styled-components"
+
+const TermsPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <Navigation />
+    <TermsWrapper>
+      <Container>
+        <h2>Terms of Service</h2>
+        <h4>1. Terms & Introduction</h4>
+        <p>
+          These terms of service (the "Terms of Service") describe your rights
+          and responsibilities when using the Mail Masker application (referred
+          to as the "Website" when accessed via a browser, and the "Platform"
+          when accessed via any means). These Terms of Service, our Privacy
+          Policy, and any other terms agreed to in writing by you and us
+          (referred to collectively as the "Terms") govern your use of the
+          Platform. By accessing or using the Platform, you acknowledge that you
+          have read, understood, and agreed to be legally bound by and comply
+          with these Terms. If you do not agree with any of these Terms, you may
+          not use the Platform. You understand and agree that satisfying the
+          above requirements does not guarantee that you will access, use, or
+          provide services through the Platform. In these Terms, the terms "you"
+          and "yours" refer to the entity on whose behalf these Terms are
+          entered into. The terms "we", "our", "us", "Mail Masker" and "Dewpoint
+          Solutions" refer to Dewpoint Solutions, Inc. Even though you may have
+          arrived to the Platform through a website or mobile application
+          operated or controlled by a third party, including by an affiliate of
+          Mail Masker, you understand and agree that these Terms are entered
+          into between you and Dewpoint Solutions. You also understand and agree
+          that the Platform and any services provided through these Terms are
+          provided solely by Dewpoint Solutions, and no other parent,
+          subsidiary, or affiliate of Dewpoint Solutions. Any person using or
+          accessing the Platform for or on behalf of you represents and warrants
+          that they have the authority to use the Website and agree to these
+          Terms on your behalf.
+        </p>
+        <p>
+          If you do not agree with any of these terms, you are prohibited from
+          using or accessing this site. The materials contained in this website
+          are protected by applicable copyright and trademark law.
+        </p>
+        <h4>2. Use License</h4>
+        <ol type="a">
+          <li>
+            Permission is granted to temporarily download one copy of the
+            materials (information or software) on the Website for personal,
+            non-commercial transitory viewing only. This is the grant of a
+            license, not a transfer of title, and under this license you may
+            not:
+            <ol type="i">
+              <li>
+                Use or attempt to use the Platform via any account that is not
+                your own or in any other way that is not personally and uniquely
+                identifiable to you;
+              </li>
+              <li>
+                Use or attempt to use the Platform to access any information, to
+                perform any function, or to accomplish any other purpose for
+                which you do not have authorization;
+              </li>
+              <li>
+                Access or use the Platform in any manner or for any purpose that
+                infringes, misappropriates, or otherwise violates any
+                intellectual property right or other right of any third party,
+                or that violates any applicable local, state, federal, or other
+                law or regulation, or that is prohibited by these Terms;
+              </li>
+              <li>
+                License, sublicense, sell, resell, transfer, assign, distribute,
+                or otherwise commercially exploit or make available to any third
+                party the Platform or related materials in any way;
+              </li>
+              <li>
+                Use or access the Platform to create or develop competing
+                products or services or for any other purpose that is to
+                Dewpoint Solutions's detriment or commercial disadvantage;
+              </li>
+              <li>
+                Take any action or use the Platform in any manner which could
+                damage, destroy, disrupt, disable, impair, overburden, interfere
+                with, or otherwise impede or harm in any manner our Platform or
+                any content, in whole or in part;
+              </li>
+              <li>
+                Disrupt, interfere with, violate the security of, or attempt to
+                gain unauthorized access to our Platform;
+              </li>
+              <li>
+                Bypass, breach, avoid, remove, deactivate, impair, descramble,
+                or otherwise circumvent any security device, protection, or
+                technological measure implemented by Dewpoint Solutions or any
+                of our service providers to protect our Platform;
+              </li>
+              <li>
+                Input, upload, transmit, distribute, or otherwise run or
+                propagate any virus, application, Trojan horse, or any other
+                harmful computer code that could damage or alter a computer,
+                portable device, computer network, communication network, data,
+                or our Platform, or any other system, device, or property;
+              </li>
+              <li>
+                Remove, delete, alter, or obscure any trademarks,
+                specifications, warranties, or disclaimers, or any copyright,
+                trademark, patent, or other intellectual property or proprietary
+                rights notices from our Platform or any content made available
+                to you on or through our Platform;
+              </li>
+              <li>
+                Use any manual process or automated device to monitor or copy
+                any content made available on or through our Platform for any
+                unauthorized purpose except as permitted in Section ‎‎IX; or
+              </li>
+              <li>
+                Copy, duplicate, download, store in a retrieval system, publish,
+                transmit, or otherwise reproduce, transfer, distribute, store,
+                disseminate, aggregate, use as a component of or as the basis
+                for a database, or otherwise use in any form or by any means any
+                data, text, reports, or other materials related to Dewpoint
+                Solutions or third-party content from the Platform.
+              </li>
+            </ol>
+          </li>
+          <li>
+            This license shall automatically terminate if you violate any of
+            these restrictions and may be terminated by Dewpoint Solutions at
+            any time. Upon terminating your viewing of these materials or upon
+            the termination of this license, you must destroy any downloaded
+            materials in your possession whether in electronic or printed
+            format.
+          </li>
+        </ol>
+        <h4>3. Disclaimer</h4>
+        <ol type="a">
+          <li>
+            The materials on Dewpoint Solutions's website are provided on an 'as
+            is' basis. Dewpoint Solutions makes no warranties, expressed or
+            implied, and hereby disclaims and negates all other warranties
+            including, without limitation, implied warranties or conditions of
+            merchantability, fitness for a particular purpose, or
+            non-infringement of intellectual property or other violation of
+            rights.
+          </li>
+          <li>
+            Further, Dewpoint Solutions does not warrant or make any
+            representations concerning the accuracy, likely results, or
+            reliability of the use of the materials on its website or otherwise
+            relating to such materials or on any sites linked to this site.
+          </li>
+        </ol>
+        <h4>4. Limitations</h4>
+        <p>
+          In no event shall Dewpoint Solutions or its suppliers be liable for
+          any damages (including, without limitation, damages for loss of data
+          or profit, or due to business interruption) arising out of the use or
+          inability to use the materials on Dewpoint Solutions's website, even
+          if Dewpoint Solutions or a Dewpoint Solutions, Inc. authorized
+          representative has been notified orally or in writing of the
+          possibility of such damage. Because some jurisdictions do not allow
+          limitations on implied warranties, or limitations of liability for
+          consequential or incidental damages, these limitations may not apply
+          to you.
+        </p>
+        <h4>5. Accuracy of materials</h4>
+        <p>
+          The materials appearing on Dewpoint Solutions's website could include
+          technical, typographical, or photographic errors. Dewpoint Solutions,
+          Inc. does not warrant that any of the materials on its website are
+          accurate, complete or current. Dewpoint Solutions may make changes to
+          the materials contained on its website at any time without notice.
+          However Dewpoint Solutions does not make any commitment to update the
+          materials.
+        </p>
+        <h4>6. Links</h4>
+        <p>
+          Dewpoint Solutions has not reviewed all of the sites linked to its
+          website and is not responsible for the contents of any such linked
+          site. The inclusion of any link does not imply endorsement by Dewpoint
+          Solutions, Inc. of the site. Use of any such linked website is at the
+          user's own risk.
+        </p>
+        <h4>7. Modifications</h4>
+        <p>
+          Dewpoint Solutions may revise these terms of service for its website
+          at any time without notice. By using this website you are agreeing to
+          be bound by the then current version of these terms of service.
+        </p>
+        <h4>8. Governing Law</h4>
+        <p>
+          These terms and conditions are governed by and construed in accordance
+          with the laws of the State of Arizona and United States of America and
+          you irrevocably submit to the exclusive jurisdiction of the courts in
+          that State or location.
+        </p>
+        <hr />
+        <p>
+          See an issue with these Terms of Service, or have something you'd like
+          to discuss? We'd love to discuss it with you at{" "}
+          <a href="mailto:terms@mailmasker.com">terms@mailmasker.com</a>.
+        </p>
+      </Container>
+    </TermsWrapper>
+    <Footer />
+  </Layout>
+)
+
+export default TermsPage
+
+const TermsWrapper = styled.header`
+  background-color: #f8f8f8;
+  padding: 160px 0 160px 0;
+  position: relative;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0 calc(100% - 5vw));
+  @media (max-width: ${props => props.theme.screen.md}) {
+  }
+`
