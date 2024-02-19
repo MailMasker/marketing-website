406a4920e79cfb69e3d4f4e50acecc42af99537b
Jonathan Sibley <jonsibley@gmail.com>
2024-02-19 11:08:20 +0100

    update deployment script to not use vault, include cloudfront invalidation

--- deploy-dev.sh
+++ deploy-dev.sh
@@ -3,4 +3,4 @@
 rm -rf .cache
 
 ACKEE_DOMAIN_ID=76d016b5-a0a7-44b2-b090-f19344833cf6 yarn build
-aws-vault exec email-forwarder-dev aws s3 sync public/ s3://email-forwarder-www-dev
+aws s3 sync public/ s3://email-forwarder-www-dev
--- deploy-prod.sh
+++ deploy-prod.sh
@@ -4,4 +4,6 @@
 rm -rf .cache
 
 ACKEE_DOMAIN_ID=b79f0d27-bb42-4711-a8f2-58a666975d7e yarn build
-aws-vault exec email-forwarder-prod aws s3 sync public/ s3://email-forwarder-www-prod
+aws s3 sync public/ s3://email-forwarder-www-prod
+
+aws cloudfront create-invalidation --distribution-id E1C10OZ7UCHGW4 --paths "/*" | cat
